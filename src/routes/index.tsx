8762202e58c0a661809a856d9739e05dbc14d4be
Juno Kim <lippoint.surf0622@gmail.com>
2022-06-30 16:59:07 +0900

    비밀번호 찾기 페이지 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { Route, HashRouter, Routes } from 'react-router-dom';
-import Splash from '../screens/Splash';
-import Login from '../screens/Login';
-import EmailLogin from '../screens/Login/EmailLogin';
-import ChangePwd from '../screens/Login/ChangePwd';
-import Code from '../screens/Code';
-import List from '../screens/Conference/List';
-import SessionDetail from '../screens/Conference/SessionDetail';
-import ConferenceDetail from '../screens/Conference/ConferenceDetail';
-import Terms from '../screens/Register/Terms';
-import Register from '../screens/Register/Register';
-import RegisterCode from '../screens/Register/RegisterCode';
-import Settings from '../screens/Settings';
-import TermsOfUse from '../screens/TermsOfUse';
-import PrivacyPolicy from '../screens/TermsOfUse/PrivacyPolicy';
+import React from 'react'
+import { Route, HashRouter, Routes } from 'react-router-dom'
+import Splash from '../screens/Splash'
+import Login from '../screens/Login'
+import EmailLogin from '../screens/Login/EmailLogin'
+import ChangePwd from '../screens/Login/ChangePwd'
+import FindPwd from '../screens/Login/FindPwd'
+import Code from '../screens/Code'
+import List from '../screens/Conference/List'
+import SessionDetail from '../screens/Conference/SessionDetail'
+import ConferenceDetail from '../screens/Conference/ConferenceDetail'
+import Terms from '../screens/Register/Terms'
+import Register from '../screens/Register/Register'
+import RegisterCode from '../screens/Register/RegisterCode'
+import Settings from '../screens/Settings'
+import TermsOfUse from '../screens/TermsOfUse'
+import PrivacyPolicy from '../screens/TermsOfUse/PrivacyPolicy'
 
-export default function Pages() {
+export default function Pages () {
   return (
     <HashRouter>
       <Routes>
@@ -23,6 +24,7 @@ export default function Pages() {
         <Route path='/login' element={<Login />} />
         <Route path='/emailLogin' element={<EmailLogin />} />
         <Route path='/changePwd' element={<ChangePwd />} />
+        <Route path='/findPwd' element={<FindPwd />} />
         <Route path='/code' element={<Code />} />
         <Route path='/list' element={<List />} />
         <Route path='/sessionDetail' element={<SessionDetail />} />
@@ -35,5 +37,5 @@ export default function Pages() {
         <Route path='/privacyPolicy' element={<PrivacyPolicy />} />
       </Routes>
     </HashRouter>
-  );
+  )
 }
