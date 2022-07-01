7903d333bc996582963e628d178ebcfac40fdf63
Juno Kim <lippoint.surf0622@gmail.com>
2022-07-01 11:46:49 +0900

    electron일 경우 시스템 설정 locale 가져와서 앱 기본 언어로 설정 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { IntlProvider } from 'react-intl';
 import '../src/styles/global.css';
 import Pages from './routes';
@@ -7,27 +7,59 @@ import ko from './language/ko-KR';
 import { useDispatch, useSelector } from 'react-redux';
 import { localeUpdate } from './store/localeReducer';
 import { RootState } from './store';
+import appRuntime from './appRuntime';
 
 function App() {
   const dispatch = useDispatch();
   const { locale } = useSelector((state: RootState) => state.locale);
+  const [systemLang, setSystemLang] = useState<string>('');
 
-  const message = locale === 'ko' ? ko : en;
+  // web 테스트시 아래 함수 실행
+  // const localeUpdateHandler = () => {
+  //   appRuntime.send('lang', null);
 
+  //   const naviLocale = navigator.language;
+
+  //   let filterLocale = naviLocale;
+  //   if (naviLocale === 'en-US') {
+  //     filterLocale = 'en';
+  //   }
+  //   dispatch(localeUpdate(filterLocale));
+  // };
+
+  // useEffect(() => {
+  //   localeUpdateHandler();
+  //   return () => localeUpdateHandler();
+  // }, []);
+
+  // electron 모드에선 아래 함수 실행
   const localeUpdateHandler = () => {
-    const naviLocale = navigator.language;
-
-    let filterLocale = naviLocale;
-    if (naviLocale === 'en-US') {
+    let filterLocale = systemLang;
+    if (systemLang !== 'ko') {
       filterLocale = 'en';
     }
     dispatch(localeUpdate(filterLocale));
   };
 
   useEffect(() => {
+    appRuntime.send('lang', null);
+    return () => appRuntime.send('lang', null);
+  }, []);
+
+  useEffect(() => {
     localeUpdateHandler();
     return () => localeUpdateHandler();
-  }, []);
+  }, [systemLang]);
+
+  appRuntime.on('lang', (event: any, data: any) => {
+    // console.log('electron lang event ?', event);
+    console.log('electron lang data ?', data);
+    setSystemLang(data);
+  });
+
+  // console.log('current Locale ?', locale);
+
+  const message = locale === 'ko' ? ko : en;
 
   return (
     <React.StrictMode>
