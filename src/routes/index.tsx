aabafbc9d01a8324d571d52740b77bfd769c80ae
Juno Kim <lippoint.surf0622@gmail.com>
2022-07-06 17:52:08 +0900

    카카오 로그인 부분 작업중

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import { Route, HashRouter, Routes } from 'react-router-dom'
-import Splash from '../screens/Splash'
-import Login from '../screens/Login'
-import EmailLogin from '../screens/Login/EmailLogin'
-import ChangePwd from '../screens/Login/ChangePwd'
-import FindPwd from '../screens/Login/FindPwd'
-import Kakao from '../screens/Login/SNS/Kakao'
-import Code from '../screens/Code'
-import List from '../screens/Conference/List'
-import SessionDetail from '../screens/Conference/SessionDetail'
-import ConferenceDetail from '../screens/Conference/ConferenceDetail'
-import Terms from '../screens/Register/Terms'
-import Register from '../screens/Register/Register'
-import RegisterCode from '../screens/Register/RegisterCode'
-import Settings from '../screens/Settings'
-import TermsOfUse from '../screens/TermsOfUse'
-import PrivacyPolicy from '../screens/TermsOfUse/PrivacyPolicy'
+import React from 'react';
+import { Route, HashRouter, Routes } from 'react-router-dom';
+import Splash from '../screens/Splash';
+import Login from '../screens/Login';
+import EmailLogin from '../screens/Login/EmailLogin';
+import ChangePwd from '../screens/Login/ChangePwd';
+import FindPwd from '../screens/Login/FindPwd';
+import Kakao from '../screens/Login/SNS/Kakao';
+import Code from '../screens/Code';
+import List from '../screens/Conference/List';
+import SessionDetail from '../screens/Conference/SessionDetail';
+import ConferenceDetail from '../screens/Conference/ConferenceDetail';
+import Terms from '../screens/Register/Terms';
+import Register from '../screens/Register/Register';
+import RegisterCode from '../screens/Register/RegisterCode';
+import Settings from '../screens/Settings';
+import TermsOfUse from '../screens/TermsOfUse';
+import PrivacyPolicy from '../screens/TermsOfUse/PrivacyPolicy';
 
-export default function Pages () {
+export default function Pages() {
   return (
     <HashRouter>
       <Routes>
         <Route path='/' element={<Splash />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/emailLogin' element={<EmailLogin />} />
         <Route path='/auth/kakao/callback' element={<Kakao />} />
+        <Route path='/emailLogin' element={<EmailLogin />} />
         <Route path='/changePwd' element={<ChangePwd />} />
         <Route path='/findPwd' element={<FindPwd />} />
         <Route path='/code' element={<Code />} />
@@ -39,5 +39,5 @@ export default function Pages () {
         <Route path='/privacyPolicy' element={<PrivacyPolicy />} />
       </Routes>
     </HashRouter>
-  )
+  );
 }
