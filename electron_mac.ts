0af8524118192c442c35bf8c711390f42929955a
Juno Kim <lippoint.surf0622@gmail.com>
2022-08-05 10:55:46 +0900

    electron mac용

---
+++ electron_mac.ts
@@ -0,0 +1,438 @@
+import {
+  app,
+  BrowserWindow,
+  Menu,
+  ipcMain,
+  shell,
+  dialog,
+  screen,
+} from 'electron';
+import * as isDev from 'electron-is-dev';
+import * as path from 'path';
+const fs = require('fs');
+const os = require('os');
+const url = require('url');
+import { download } from 'electron-dl';
+import { isContext } from 'vm';
+// const Env = JSON.parse(fs.readFileSync(`${__dirname}/env.json`));
+
+const MIN_MODE_MAX_HEIGHT_SMALL = 250;
+const MIN_MODE_MAX_HEIGHT_BIG = 285;
+const MIN_WIDTH = 330;
+const MIN_HEIGHT = 600;
+const MAX_WIDTH = 380;
+const MAX_HEIGHT = 780;
+
+let mainWindow: Electron.BrowserWindow | null;
+const REDIRECT_URL = 'https://change-all.com/listen_auth_callback';
+const USER_AGENT = { userAgent: 'Chrome' };
+
+let isMinMode = false; // 일렉트론 접힌 화면 모드
+let resizeWidth = 380;
+let resizeHeight = 780;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    titleBarStyle: 'customButtonsOnHover',
+    frame: false,
+    transparent: true,
+    width: resizeWidth,
+    height: resizeHeight,
+    minWidth: MIN_WIDTH,
+    minHeight: MIN_HEIGHT,
+    maxWidth: MAX_WIDTH,
+    maxHeight: isMinMode ? MIN_MODE_MAX_HEIGHT_BIG : MAX_HEIGHT,
+    title: 'MOLI-Listener',
+    resizable: !isMinMode,
+    autoHideMenuBar: true,
+    hasShadow: true,
+    icon: path.join(
+      app.getAppPath(),
+      isDev ? '/icons/png/64x64.png' : '/build/icons/png/64x64.png'
+    ),
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      enableRemoteModule: true,
+      webSecurity: false,
+      preload: path.join(
+        app.getAppPath(),
+        isDev ? '/preload.js' : '/build/preload.js'
+      ),
+    },
+  });
+
+  mainWindow.isResizable();
+  mainWindow.setResizable(true);
+
+  let winSize = mainWindow.getSize();
+  console.log('winSize ::', winSize);
+
+  let indexPath;
+
+  indexPath = url.format({
+    protocol: 'file:',
+    pathname: path.join(
+      app.getAppPath(),
+      isDev ? '/index.html' : '/build/index.html'
+    ),
+    slashes: true,
+  });
+
+  app.allowRendererProcessReuse = false;
+
+  // mainWindow.loadURL(renderPath);
+  // console.log('electron isDev ?', isDev);
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000');
+    // mainWindow.loadURL(indexPath);
+    mainWindow.webContents.openDevTools(); // 개발자 툴 오픈
+  } else {
+    mainWindow.loadURL('https://change-all.com/listen_desk');
+  }
+  // setupPushReceiver(mainWindow.webContents);
+
+  // 기본 메뉴 숨기기
+  mainWindow.setMenuBarVisibility(false);
+
+  // 렌더러 내부 href링크 외부 브라우저로 열리게 처리
+  const handleRedirect = (e: any, url: string) => {
+    if (url != mainWindow.webContents.getURL()) {
+      e.preventDefault();
+      shell.openExternal(url);
+    }
+  };
+
+  mainWindow.webContents.on('will-navigate', handleRedirect);
+  mainWindow.webContents.on('new-window', handleRedirect);
+
+  // mainWindow.maximize();
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  mainWindow.on('resize', function () {
+    let size = mainWindow.getSize();
+    resizeWidth = size[0];
+    resizeHeight = size[1];
+
+    // 플레이어 축소모드이면서 width가 380 보다 작을 때
+    if (isMinMode && resizeWidth < MAX_WIDTH) {
+      mainWindow.setMinimumSize(MIN_WIDTH, MIN_MODE_MAX_HEIGHT_SMALL);
+      mainWindow.setMaximumSize(MAX_WIDTH, MIN_MODE_MAX_HEIGHT_SMALL);
+      mainWindow.setSize(resizeWidth, MIN_MODE_MAX_HEIGHT_SMALL, true);
+    }
+
+    // 플레이어 축소모드이면서 width가 380 보다 같거나 클 때
+    if (isMinMode && resizeWidth >= MAX_WIDTH) {
+      mainWindow.setMinimumSize(MIN_WIDTH, MIN_MODE_MAX_HEIGHT_BIG);
+      mainWindow.setMaximumSize(MAX_WIDTH, MIN_MODE_MAX_HEIGHT_BIG);
+      mainWindow.setSize(resizeWidth, MIN_MODE_MAX_HEIGHT_BIG, true);
+    }
+  });
+}
+
+// console.log('screen.getPrimaryDisplay() ?', screen.getPrimaryDisplay());
+console.log('isMinMode ?', isMinMode);
+
+// 리사이징 기능
+ipcMain.on('frameMin', (event, data) => {
+  // mainWindow.unmaximize();
+  // mainWindow.setMinimumSize(380, 250);
+  // mainWindow.setSize(380, 285, true);
+  isMinMode = true;
+  mainWindow.setMinimumSize(MIN_WIDTH, MIN_MODE_MAX_HEIGHT_BIG);
+  mainWindow.setMaximumSize(MAX_WIDTH, MIN_MODE_MAX_HEIGHT_BIG);
+  mainWindow.setSize(resizeWidth, MIN_MODE_MAX_HEIGHT_BIG, true);
+  // event.sender.send('isFrameMin', true);
+  // event.sender.send('isFrameWide', false);
+});
+
+ipcMain.on('frameWide', (event, data) => {
+  // mainWindow.maximize();
+  isMinMode = false;
+  mainWindow.setMinimumSize(MIN_WIDTH, MIN_HEIGHT);
+  mainWindow.setMaximumSize(MAX_WIDTH, MAX_HEIGHT);
+  mainWindow.setSize(resizeWidth, MAX_HEIGHT, true);
+  // event.sender.send('isFrameWide', true);
+  // event.sender.send('isFrameMin', false);
+});
+
+// 브라우저 메뉴창 없애기
+Menu.setApplicationMenu(null);
+
+// SNS Kakao
+ipcMain.on('kakaoLogin', (event, args) => {
+  let loginWindow = new BrowserWindow({
+    width: 380,
+    height: 500,
+    show: false,
+    parent: mainWindow,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+
+  let restAPIKey = '6fde81df196e383578c4a91e894b0741';
+  let apiUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${restAPIKey}&redirect_uri=${REDIRECT_URL}&response_type=code`;
+
+  loginWindow.webContents.loadURL(apiUrl, USER_AGENT);
+
+  // 카카오 앱 내 실행시 loginWindow.webContents.userAgent =
+  //   'Mozilla/5.0 (Android; Mobile; rv:13.0) Gecko/13.0 Firefox/13.0 KAKAOTALK';
+
+  loginWindow.on('ready-to-show', () => {
+    loginWindow.show();
+  });
+
+  let kakaoCode = '';
+
+  loginWindow.webContents.on(
+    'will-redirect',
+    (event: any, oldUrl: any, newUrl: any) => {
+      const url = new URL(oldUrl);
+      const urlParams = url.searchParams;
+      kakaoCode = urlParams.get('code');
+    }
+  );
+
+  loginWindow.webContents.on('did-finish-load', () => {
+    console.log('did-finish-load kakaoCode ??', kakaoCode);
+    if (kakaoCode) {
+      console.log('electron kakaoCode :', kakaoCode);
+
+      event.sender.send('kakaoLoginCode', kakaoCode);
+      loginWindow.close();
+    }
+  });
+});
+
+// SNS Naver
+ipcMain.on('naverLogin', (event, args) => {
+  let loginWindow = new BrowserWindow({
+    width: 380,
+    height: 500,
+    show: false,
+    parent: mainWindow,
+  });
+
+  let clientId = 'ZIi4Wpw4nc4fgcRrWh7k';
+  let clientSecret = 'NXykZyW6Ib';
+  let state = 'naver';
+  let apiUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${clientId}&display=popup&redirect_uri=${REDIRECT_URL}&state=${state}&scope=basic_profile`;
+
+  loginWindow.webContents.loadURL(apiUrl, USER_AGENT);
+
+  // 네이버 앱 내 실행시 loginWindow.webContents.userAgent =
+  //   'Mozilla/5.0 (iPhone; CPU iPhone OS like Mac OS X) AppleWebKit/605.1.15 NAVER(inapp; search; 620; 10.10.2; XR)';
+
+  loginWindow.on('ready-to-show', () => {
+    loginWindow.show();
+  });
+
+  let naverOauthCode = '';
+
+  loginWindow.webContents.on('will-navigate', (event, newUrl) => {
+    console.log('naver will navigate New Url ::', newUrl);
+
+    const url = new URL(newUrl);
+    const urlParams = url.searchParams;
+    naverOauthCode = urlParams.get('oauth_token');
+  });
+
+  loginWindow.webContents.on('did-finish-load', () => {
+    console.log('did-finish-load naverOauthCode ??', naverOauthCode);
+    if (naverOauthCode) {
+      event.sender.send('naverLoginCode', naverOauthCode);
+      loginWindow.close();
+    }
+  });
+});
+
+// SNS Google
+ipcMain.on('googleLogin', (event, args) => {
+  let loginWindow = new BrowserWindow({
+    width: 380,
+    height: 500,
+    show: true,
+    parent: mainWindow,
+  });
+
+  let clientId =
+    '759277572836-jj5c320hk1io87gvj1o4n5ggemh21jpk.apps.googleusercontent.com';
+  let scope =
+    'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
+
+  let apiUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${REDIRECT_URL}&scope=${scope}&response_type=code`;
+
+  loginWindow.webContents.loadURL(apiUrl, {
+    userAgent:
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0',
+  });
+
+  // loginWindow.webContents.userAgent =
+  //   'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0';
+
+  loginWindow.on('ready-to-show', () => {
+    loginWindow.show();
+  });
+
+  let googleCode: string = '';
+
+  // loginWindow.webContents.on(
+  //   'will-redirect',
+  //   (event: any, oldUrl: any, newUrl: any) => {
+  //     console.log('googleLogin oldUrl ??', oldUrl);
+
+  //     const url = new URL(oldUrl);
+  //     const urlParams = url.searchParams;
+  //     googleCode = urlParams.get('code');
+  //   }
+  // );
+
+  // loginWindow.webContents.on('will-navigate', (event, newUrl) => {
+  //   const url = new URL(newUrl);
+  //   const urlParams = url.searchParams;
+  //   googleCode = urlParams.get('code');
+  // });
+
+  loginWindow.webContents.on('did-navigate', (event, newUrl) => {
+    console.log('google did navigate New Url ::', newUrl);
+
+    const url = new URL(newUrl);
+    const urlParams = url.searchParams;
+    googleCode = urlParams.get('code');
+  });
+
+  loginWindow.webContents.on('did-finish-load', () => {
+    if (googleCode) {
+      event.sender.send('googleLoginCode', googleCode);
+      loginWindow.close();
+    }
+  });
+});
+
+// SNS facebook
+ipcMain.on('fbLogin', (event, args) => {
+  let loginWindow = new BrowserWindow({
+    width: 380,
+    height: 500,
+    show: false,
+    // modal: true,
+    parent: mainWindow,
+    webPreferences: {
+      webSecurity: false,
+      plugins: true,
+    },
+  });
+
+  let clientId = '438180334454176';
+  let apiUrl = `https://www.facebook.com/v14.0/dialog/oauth?client_id=${clientId}&redirect_uri=${REDIRECT_URL}&state=f11&resource_type=token&scope=public_profile,email`;
+
+  loginWindow.webContents.loadURL(apiUrl, USER_AGENT);
+
+  loginWindow.on('ready-to-show', () => {
+    loginWindow.show();
+  });
+
+  let facebookCode: string = '';
+  loginWindow.webContents.on(
+    'will-redirect',
+    (event: any, oldUrl: any, newUrl: any) => {
+      const url = new URL(oldUrl);
+      const urlParams = url.searchParams;
+      facebookCode = urlParams.get('code');
+    }
+  );
+
+  loginWindow.webContents.on('did-finish-load', () => {
+    if (facebookCode) {
+      event.sender.send('fbLoginCode', facebookCode);
+      loginWindow.close();
+    }
+  });
+});
+
+// 창 닫기
+ipcMain.on('windowClose', (event, data) => {
+  console.log('app quit?', data);
+  app.exit();
+});
+
+// 창 내리기
+ipcMain.on('windowMinimize', (event, data) => {
+  mainWindow.minimize();
+});
+
+// 토큰 가져오기
+let fcmToken = '';
+ipcMain.on('fcmToken', (event, data) => {
+  console.log('electron token data', data);
+  fcmToken = data;
+  event.sender.send('electronToken', data);
+});
+
+ipcMain.on('callToken', (event, data) => {
+  event.sender.send('electronToken', fcmToken);
+});
+
+// 사운드 카운트 받기
+ipcMain.on('sound_count', (event, data) => {
+  event.sender.send('get_sound_count', data);
+});
+
+// 사운드 VOLUMNE 설정
+ipcMain.on('sound_volume', (event, data) => {
+  event.sender.send('get_sound_vol', data);
+});
+
+// 메인프로세서 종료
+ipcMain.on('closeWindow', (event, data) => {
+  mainWindow = null;
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+ipcMain.on('lang', (event, data) => {
+  event.sender.send('lang', app.getLocale());
+});
+
+if (process.platform === 'win32') {
+  app.setAppUserModelId('MOLI');
+}
+
+ipcMain.handle('quit-app', () => {
+  app.quit();
+});
+
+app.on('ready', createWindow);
+
+app.on('open-url', (event, url) => {
+  // dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`);
+  shell.openExternal(url);
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// 파일 다운로드(url 이미지)
+ipcMain.on('download', async (event, { url, filename }) => {
+  const win = BrowserWindow.getFocusedWindow();
+  // console.log(await download(win, url));
+  await download(win, url, {
+    saveAs: true,
+    filename,
+    openFolderWhenDone: true,
+  });
+});
